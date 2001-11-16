9b0c98af8d6c79d7987b4074885d6cb8436b822f
Rainer Clasen <bj@zuto.de>
2001-11-16 21:59:50 +0000

    new xtitle view combining all inwolved tables

--- tables/music/title.sql
+++ tables/music/title.sql
@@ -43,6 +43,36 @@ GRANT SELECT ON mus_title_id_seq TO PUBLIC;
 GRANT all ON mus_title TO GROUP dudl;
 GRANT all ON mus_title_id_seq TO GROUP dudl;
 
+CREATE VIEW mus_xtitle AS
+SELECT
+	aa.id			AS album_artist_id,
+	aa.nname		AS album_artist,
+	t.album_id,
+	a.album,
+	t.id,
+	t.nr,
+	t.title,
+	ta.id			AS title_artist_id,
+	ta.nname		AS title_artist,
+	t.genres,
+	u.collection,
+	u.colnum,
+	f.dir,
+	f.fname
+FROM
+	mus_title		t,
+	mus_album		a,
+	mus_artist		ta,
+	mus_artist		aa,
+	stor_file		f,
+	stor_unit		u
+WHERE
+	t.artist_id = ta.id AND
+	t.album_id = a.id AND
+	a.artist_id = aa.id AND
+	t.id = f.titleid AND
+	f.unitid = u.id;
+
 COMMIT;
 
 
