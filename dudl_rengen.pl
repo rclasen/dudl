3b6bd446ea519a75979832fc86e916986386ff78
Rainer Clasen <bj@zuto.de>
2001-11-30 14:38:46 +0000

    rewrite use Dudl::Job::Rename

--- dudl_rengen.pl
+++ dudl_rengen.pl
@@ -1,48 +1,95 @@
 #!/usr/bin/perl -w
 
-# v0.0.1 - 2000-12-26
-#	* release
+# TODO: optionally suggest based on manually specified regexp
 
+use strict;
+use Dudl;
+use Dudl::Suggester;
+use MP3::Tag;
+use Dudl::Job::Rename;
 
-# FIXME
+# TODO: getopt
+# TODO: merge with dudl_musgen?
+
+my $opt_max = 2;
+my $opt_minscore = 6;
+my $opt_genres = "";
+
+my @files;
+
 # decide where to look for filenames and fill an array
 if( defined $ARGV[0] ){
 	if( -d $ARGV[0] ){
+		my $dir = $ARGV[0];
 		# - readdir $ARGV[0]
-		opendir( DIR, $ARGV[0] ) || die "cannot opendir \"$ARGV[0]\": $! ";
+		opendir( DIR, $dir ) || die "cannot opendir \"$dir\": $! ";
 		while( defined( $_ = readdir( DIR )) ){
-			&print_line( $_ );
+			push @files, "$dir/$_";
 		}
 		closedir( DIR );
 	} else {
 		# - process all of @ARGV
-		foreach $_ ( @ARGV ){
-			&print_line( $_ );
-		}
+		push @files, @ARGV;
 	}
 } else {
 	# - read stdin
 	while( <STDIN> ){
-		&print_line( $_ );
+		chomp;
+		push @files, $_;
 	}
 }
 
+my $dudl = new Dudl;
+my $job = new Dudl::Job::Rename;
 
-# FIXME
-# keep compatibility with filenames != /<basename>_<NO>.(wav|mp3)/i
+$job->add_album(
+	name	=> "",
+	artist	=> "VARIOUS",
+	);
 
-# FIXME
-# sort the array
-# for each filename 
-# 	skip non(mp3|wav) filenames
-# 	skip if duplicate  mp3/wav filenames
-# 	if last basename != current basename print Album-fileds
-# 	print track fileds
-sub print_line {
-	if( /^(.*_(\d\d)).(wav|mp3)$/i ){
-		print "\n\n\nG \nA \n#S\n\n" if( $2 == 1 ); 
-		print "N$2\nF $1.mp3\nT \n\n";
-	} else {
-		print( STDERR "unknown file: $_" );
+foreach my $f ( @files ){
+	$job->add_file( mp3 => $f );
+
+	my $sug = new Dudl::Suggester( $dudl, $f );
+
+	$sug->add( source => "empty" );
+	$sug->add_stor( $f );
+
+	my $id3 = new MP3::Tag( $f );
+	if( $id3 ){
+		foreach my $tag ( $id3->get_tags ){
+			$sug->add( 
+				source		=> $tag,
+				artist		=> $id3->{$tag}->artist,
+				album		=> $id3->{$tag}->album,
+				title		=> $id3->{$tag}->song,
+				titlenum	=> $id3->{$tag}->track,
+				);
+		}
+	}
+
+	$sug->order;
+
+	my $dat;
+	my $sugnum = 0;
+	while( (!$opt_max || $sugnum < $opt_max ) &&
+	    defined( $dat = $sug->get ) ){
+		
+		next if $sugnum && $dat->{sug_quality} < $opt_minscore;
+
+		$job->add_title( 
+			source	=> $dat->{source}.
+				" score: ". $dat->{sug_quality},
+			num	=> $dat->{titlenum},
+			name	=> $dat->{title},
+			artist	=> $dat->{artist},
+			genres	=> $opt_genres,
+			);
+
+		$sugnum++;
 	}
 }
+
+$job->write( \*STDOUT );
+
+$dudl->done;
