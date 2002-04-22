7659c1fd75c92d4ad1774dfc70bf363282356732
Rainer Clasen <bj@zuto.de>
2002-04-22 10:15:34 +0000

    added time conversion functions

--- tables/functions.sql
+++ tables/functions.sql
@@ -26,3 +26,21 @@ AS	'BEGIN
 LANGUAGE 'plpgsql';
 
 
+-- DROP FUNCTION unix2time(integer);
+CREATE FUNCTION unix2time(INTEGER)
+RETURNS TIMESTAMP
+AS '
+BEGIN
+	return timestamp ''epoch'' + ($1 || '' seconds'');
+END;
+' LANGUAGE 'plpgsql';
+
+
+-- DROP FUNCTION time2unix(timestamp);
+CREATE FUNCTION time2unix(TIMESTAMP)
+RETURNS INTEGER
+AS '
+BEGIN
+	return date_part(''epoch'', $1);
+END;
+' LANGUAGE 'plpgsql';
