ce5cd840616aa162262510c69172cbc543b8c1fd
Rainer Clasen <bj@zuto.de>
2002-04-28 11:54:41 +0000

    added option to strip riff header (untested)

--- test_riff.pl
+++ test_riff.pl
@@ -1,23 +1,58 @@
 #!/usr/bin/perl -w
 
-# $Id: test_riff.pl,v 1.2 2001-12-13 11:41:48 bj Exp $
+# $Id: test_riff.pl,v 1.3 2002-04-28 11:54:41 bj Exp $
 
 use strict;
 
-open F, "/vol/cd/MP3/jk0001/James Brown - Sex Machine The Very Best of James Brown/01 - Please Please Please!.mp3" or die "open failed: $!";
+my $f = shift;
+my $o = shift;
+
+# $f = "/vol/cd/MP3/jk0001/James Brown - Sex Machine The Very Best of James Brown/01 - Please Please Please!.mp3";
+open F, $f or die "open '$f' failed: $!";
 
 
 my( $head, $len, $subtype );
 my $buf;
 my $got = 1;
+my $hlen = 0;
 
-$got = read( F, $buf, 12 );
+$hlen = $got = read( F, $buf, 12 );
 ($head, $len, $subtype ) = unpack( "A4lA4", $buf );
+
+if( $head ne "RIFF" ){
+	die "no riff file";
+}
+
+print "len: $len\n";
+
 while( $got && ($got = read( F, $buf, 8 )) ){
+	$hlen += $got;
+
 	($head, $len ) = unpack( "A4l", $buf );
 	print "head=$head, len=$len\n";
 
-	last if $head=data;
+	last if $head eq "data";
+
 	$got = read( F, $buf, $len );
+	$hlen += $got;
 }
-close F;
+
+print "hlen: $hlen\n";
+
+if( ! defined $o  || -e $o ){
+	close F;
+	exit 0;
+}
+
+print "copy file to $o? (INTR to abort)";
+$_ = <STDIN>;
+
+open( O, ">$o" ) || die "cannot open output '$o': $!";
+seek F,$hlen+1,0;
+while( 0 < read( F, $buf, 10240)){
+	print O $buf;
+}
+
+close(O);
+close(F);
+
