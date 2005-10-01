3761761e50e36358e14715a3ef2a7954afbe79cd
Rainer Clasen <bj@zuto.de>
2005-10-01 13:42:09 +0000

    renamed filter -> sfilter

---
+++ tables/mserv/sfilter.sql
@@ -0,0 +1,26 @@
+BEGIN;
+
+-- saved filters
+
+CREATE SEQUENCE juke_sfilter_id_seq;
+GRANT SELECT ON juke_sfilter_id_seq TO PUBLIC;
+GRANT all ON juke_sfilter_id_seq TO GROUP dudl;
+
+CREATE TABLE juke_sfilter (
+	id		INTEGER NOT NULL
+			DEFAULT nextval('juke_sfilter_id_seq'),
+	name		VARCHAR(32),
+	sfilter		TEXT
+);
+
+GRANT SELECT ON juke_sfilter TO PUBLIC;
+GRANT all ON juke_sfilter TO GROUP dudl;
+
+-- indices
+
+CREATE UNIQUE INDEX juke_sfilter__id 
+	ON juke_sfilter(id);
+CREATE UNIQUE INDEX juke_sfilter__name 
+	ON juke_sfilter( lower(name) );
+
+COMMIT;
