00d39c95a74abcbb6fb10b3ffd1e51ff30ff11bb
Rainer Clasen <bj@zuto.de>
2006-01-12 12:40:16 +0000

    update

---
+++ test_uload.pl
@@ -0,0 +1,20 @@
+#!usr/bin/perl -w
+
+use strict;
+
+use Dudl::DB;
+use Dudl::StorUnit;
+
+my $dudl = new Dudl::DB;
+my $o;
+
+$o = Dudl::StorUnit->new( dudl => $dudl );
+$o->val( volname => "test" );
+$o->val( colnum => 1 );
+my $id = $o->save;
+
+$o = Dudl::StorUnit->load( 
+	dudl => $dudl, 
+	where => { id => $id } );
+
+$dudl->rollback;
