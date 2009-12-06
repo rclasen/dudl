4dd0894d5013fda290ddd971c3f738d0175d76fa
Rainer Clasen <bj@zuto.de>
2009-12-06 12:10:31 +0000

    fix: use gstreamer rganalysis as intended to suppress warnings

--- Dudl/Gst.pm
+++ Dudl/Gst.pm
@@ -11,12 +11,11 @@
 use strict;
 use bignum;
 
-package Dudl::Gst::Scan;
+package Dudl::Gst;
 use Carp;
 use Glib;
-use GStreamer -init; # TODO: no init, breaks ARGV
+use GStreamer qw/ -init GST_SECOND /; # TODO: no init, breaks ARGV
 use File::Temp qw( tempdir );
-use Data::Dumper;
 
 # TODO: use an own mainloop instance and don't fiddle with the global one.
 
@@ -24,22 +23,28 @@ our @ISA = qw( Exporter );
 
 sub new {
 	my $proto = shift;
+	my $a = shift || {};
+
 	my %g;
 	my $me = {
+		debug	=> 0,
+		%$a,
 		error	=> 0,
 		loop	=> new Glib::MainLoop,
 		gst	=> \%g,
+		rsegs	=> [], # raw cutter segments
+		tdat	=> [], # per title data
 		adat	=> {
 			gain	=> 0,
 			gainp	=> 0,
 		},
-		tdat	=> {},
+		dir	=> undef, # tmpdir
 	};
 	bless $me, ref $proto || $proto;
-	$me->_newfile;
 
 	$me->{dir} = tempdir( CLEANUP => 1 )
 		or croak "cannot create temp dir: $!";
+	$me->log_debug( "tmpdir: ", $me->{dir} );
 
 	( @g{qw(src dec conv gain cut sink)} ) =
 	GStreamer::ElementFactory->make(
@@ -69,29 +74,63 @@ sub new {
 	return $me;
 }
 
-sub _newfile {
-	my $me = shift;
-	$me->{tdat} = {
+sub log_debug {
+	my( $me ) = shift;
+	$me->{debug} && print STDERR @_, "\n";
+}
+
+sub album {
+	my( $me ) = @_;
+	$me->{adat};
+}
+
+sub track {
+	my( $me, $id ) = @_;
+	$me->{tdat}[$id];
+}
+
+sub tracks {
+	my( $me ) = @_;
+	$me->{tdat};
+}
+
+sub scan {
+	my( $me, $files ) = @_;
+
+	$me->{adat} = {};
+	$me->{tdat} = [];
+
+	$me->{gst}{gain}->set( "num-tracks", scalar @$files );
+	foreach my $file ( @$files ){
+		$me->_scanfile( $file ) || return;
+	}
+
+	1;
+}
+
+sub _scanfile {
+	my( $me, $file ) = @_;
+	
+	$me->log_debug( "scanning $file ..." );
+	$me->{error} = 0;
+
+	my @rsegs;
+	$me->{rsegs} = \@rsegs;
+
+	my $tdat = {
+		fname	=> $file,
+		# final pipe duration/position:
 		dur	=> 0,
-		# cutter:
+		# processed segs from cutter:
 		segs	=> [],
 		# replaygain:
 		gain	=> 0,
 		gainp	=> 0,
 	}; 
-	$me->{segs} = [];
-}
-
-
-sub scanfile {
-	my( $me, $file, $last ) = @_;
-	
-	#print STDERR "scanning $file ...\n";
-	$me->{error} = 0;
-	$me->_newfile;
+	push @{$me->{tdat}}, $tdat;
 
 	# TODO: evil hack to workaround utf8 problems
-	my $lnk = "$me->{dir}/lnk";
+	my $lnk = $me->{dir} .'/lnk';
 	unlink( $lnk );
 	if( ! symlink( $file, $lnk ) ){
 		carp "failed to create temp symlink";
@@ -99,11 +138,9 @@ sub scanfile {
 	}
 
 	$me->{gst}{src}->set( "location", $lnk );
-	$me->{gst}{gain}->set( "num-tracks", $last ? 1 : 10 );
 
 	my $to = Glib::Timeout->add( 500, \&cb_getpos, $me );
 	$me->{gst}{pipe}->set_state( "playing" );
-	$me->{gst}{gain}->set_locked_state( $last ? 1 : 0 );
 	$me->{loop}->run;
 	Glib::Source->remove( $to );
 
@@ -114,58 +151,63 @@ sub scanfile {
 	my $d = new GStreamer::Query::Duration( "time" );
 	$me->{gst}{pipe}->query( $d );
 
-	$me->{gst}{pipe}->set_state( $last ? "null" : "ready");
 
 	#print STDERR "pos=", ($p->position)[1],
 		#", dur=", ($d->duration)[1], "\n";
 
 	# TODO: hackish $p and $d access
-	if( $me->{tdat}{dur} < ($p->position)[1] ){
-		$me->{tdat}{dur} = ($p->position)[1];
+	if( $tdat->{dur} < ($p->position)[1] ){
+		$tdat->{dur} = ($p->position)[1];
 	}
-	if( $me->{tdat}{dur} < ($d->duration)[1] ){
-		$me->{tdat}{dur} = ($d->duration)[1];
+	if( $tdat->{dur} < ($d->duration)[1] ){
+		$tdat->{dur} = ($d->duration)[1];
 	}
 
-	my $segs = $me->{segs};
-	if( $#$segs < 0 ){
-		push @$segs, {
+	$me->{gst}{pipe}->set_state( "ready" );
+
+	# cutter found no segments. synthesize one segment that spans the
+	# whole file:
+	if( $#rsegs < 0 ){
+		push @rsegs, {
 			above		=> 1,
 			timestamp	=> 0,
 		}, {
 			above		=> 0,
-			timestamp	=> $me->{tdat}{dur},
+			timestamp	=> $tdat->{dur},
 		};
 
+	# synthesize (missing) leading + trailing segments:
 	} else {
-		my $first = $segs->[0];
+		my $first = $rsegs[0];
 
 		if( ! $first->{above} ){
-			unshift @$segs, {
+			unshift @rsegs, {
 				above		=> 1,
 				timestamp	=> 0,
 			};
 		}
 
-		my $last = $segs->[$#$segs];
+		my $last = $rsegs[-1];
 		if( $last->{above} ){
-			push @$segs, {
+			push @rsegs, {
 				above		=> 0,
-				timestamp	=> $me->{tdat}{dur},
+				timestamp	=> $tdat->{dur},
 			}
 		}
 	}
-	foreach my $i ( 0..$#$segs ){
-		my $seg = $segs->[$i];
-		if( $seg->{timestamp} > $me->{tdat}{dur} ){
-			$seg->{timestamp} = $me->{tdat}{dur};
+
+	# get start/end timestamps from cutter segments
+	foreach my $i ( 0..$#rsegs ){
+		my $seg = $rsegs[$i];
+		if( $seg->{timestamp} > $tdat->{dur} ){
+			$seg->{timestamp} = $tdat->{dur};
 		}
 
 		next unless $i > 0;
-		my $pseg = $segs->[$i -1];
+		my $pseg = $rsegs[$i -1];
 
 		if( $pseg->{above} && $pseg->{timestamp} < $seg->{timestamp} ){
-			push @{$me->{tdat}{segs}}, {
+			push @{$tdat->{segs}}, {
 				from	=> $pseg->{timestamp},
 				to	=> $seg->{timestamp},
 			};
@@ -173,25 +215,20 @@ sub scanfile {
 
 	}
 
-
-	return if $me->{error};
-
-	return $me->{tdat}, $me->{adat} if $last;
-	return $me->{tdat};
-}
-
-sub scanalbum {
-	my( $me ) = shift;
-	my $f = ref $_[0] eq "ARRAY" ? $_[0] : \@_;
-
-	my @ret;
-	foreach my $i ( 0.. $#$f ){
-		$me->scanfile( $f->[$i], $i == $#$f )
-			or return;
-		push @ret, $me->{tdat};
+	if( $me->{debug} ){
+		my $from = $tdat->{segs}[0]{from};
+		my $to = $tdat->{segs}[-1]{to};
+		$me->log_debug( sprintf( 'seg: %3d (%3d) %3d (%3d) %3d',
+			$from / GST_SECOND,
+			($to - $from) / GST_SECOND,
+			$to / GST_SECOND,
+			($tdat->{dur} - $to) / GST_SECOND,
+			$tdat->{dur} / GST_SECOND,
+		));
 	}
 
-	return \@ret, $me->{adat};
+	return if $me->{error};
+	1;
 }
 
 sub cb_getpos {
@@ -199,8 +236,8 @@ sub cb_getpos {
 
 	my $p = new GStreamer::Query::Position( "time" );
 	$me->{gst}{pipe}->query( $p );
-	if( ($p->position)[1] > $me->{tdat}{dur} ){
-		$me->{tdat}{dur} = ($p->position)[1];
+	if( ($p->position)[1] > $me->{tdat}[-1]{dur} ){
+		$me->{tdat}[-1]{dur} = ($p->position)[1];
 	}
 
 	return 1;
@@ -221,12 +258,12 @@ sub cb_bus_message {
 		my $t = $message->tag_list;
 
 		foreach my $n ( qw( album artist date track-number title )){
-			$me->{tdat}{$n} = $t->{$n}[0] if exists $t->{$n};
+			$me->{tdat}[-1]{$n} = $t->{$n}[0] if exists $t->{$n};
 		}
 
-		$me->{tdat}{gain} = $t->{'replaygain-track-gain'}[0]
+		$me->{tdat}[-1]{gain} = $t->{'replaygain-track-gain'}[0]
 			if exists $t->{'replaygain-track-gain'};
-		$me->{tdat}{gainp} = $t->{'replaygain-track-peak'}[0]
+		$me->{tdat}[-1]{gainp} = $t->{'replaygain-track-peak'}[0]
 			if exists $t->{'replaygain-track-peak'};
 
 		$me->{adat}{gain} = $t->{'replaygain-album-gain'}[0]
@@ -242,7 +279,8 @@ sub cb_bus_message {
 			foreach my $f ( @{$st->{fields}} ){
 				$dat{$$f[0]} = $$f[2];
 			}
-			push @{$me->{segs}}, {
+
+			push @{$me->{rsegs}}, {
 				above		=> $dat{above},
 				timestamp	=> $dat{timestamp},
 			};
@@ -255,9 +293,7 @@ sub cb_bus_message {
 
 sub DESTROY {
 	my $me = shift;
-	$me->{gst}{gain}->set_locked_state(0) if $me->{gst}{gain};
 	$me->{gst}{pipe}->set_state("null") if $me->{gst}{pipe};
-	$me->{gst}{gain}->set_state("null") if $me->{gst}{gain};
 }
 
 
